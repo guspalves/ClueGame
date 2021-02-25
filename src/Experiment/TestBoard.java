37025a189a1aa8b288121e15a53a858723d70cc5
guspalves <gus.alves@hotmail.com>
2021-02-24 22:26:12 -0700

    Creating and implementing TestBoard.java

---
+++ src/Experiment/TestBoard.java
@@ -0,0 +1,23 @@
+package Experiment;
+
+import java.util.*;
+
+public class TestBoard {
+	
+	// Constructor
+	public TestBoard() {
+		
+	}
+	
+	public void calcTargets(TestBoardCell startCell, int pathlength) {
+		return;
+	}
+	
+	public Set<TestBoardCell> getTargets(){
+		return null;
+	}
+	
+	public TestBoardCell getCell(int row, int col) {
+		return null;
+	}
+}
