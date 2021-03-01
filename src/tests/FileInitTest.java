884452d9a2f5d9bf409ea7894ca68c023ca11c28
nterry87 <nterry@mymail.mines.edu>
2021-02-28 19:30:18 -0700

    Set up FileInitTest and implemented some tests

--- src/tests/FileInitTest.java
+++ src/tests/FileInitTest.java
@@ -1,11 +1,17 @@
 package tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import clueGame.Board;
+import clueGame.BoardCell;
+import clueGame.DoorDirection;
 
 class FileInitTest {
 
@@ -28,4 +34,54 @@ class FileInitTest {
 		board.initialize();
 	}
 
+	@Test
+	public void testRoomLabels() {
+		assertEquals("Kitchen", board.getRoom('K').getName() );
+		assertEquals("Dining Room", board.getRoom('D').getName() );
+		assertEquals("Theater", board.getRoom('T').getName() );
+		assertEquals("Bowling Alley", board.getRoom('B').getName() );
+		assertEquals("Library", board.getRoom('L').getName() );
+		assertEquals("Gym", board.getRoom('G').getName() );
+		assertEquals("Pool", board.getRoom('P').getName() );
+		assertEquals("Greenhouse", board.getRoom('G').getName() );
+		assertEquals("Bedroom", board.getRoom('R').getName() );
+	}
+	
+	@Test
+	public void testBoardDimensions() {
+		// Ensure we have the proper number of rows and columns
+		assertEquals(NUM_ROWS, board.getNumRows());
+		assertEquals(NUM_COLUMNS, board.getNumColumns());
+	}
+	
+	@Test
+	public void FourDoorDirections() {
+		BoardCell cell = board.getCell(6, 5);
+		assertTrue(cell.isDoorway());
+		assertEquals(DoorDirection.LEFT, cell.getDoorDirection());
+		cell = board.getCell(4, 11);
+		assertTrue(cell.isDoorway());
+		assertEquals(DoorDirection.UP, cell.getDoorDirection());
+		cell = board.getCell(15, 18);
+		assertTrue(cell.isDoorway());
+		assertEquals(DoorDirection.RIGHT, cell.getDoorDirection());
+		cell = board.getCell(17, 13);
+		assertTrue(cell.isDoorway());
+		assertEquals(DoorDirection.DOWN, cell.getDoorDirection());
+		// Test that walkways are not doors
+		cell = board.getCell(17, 12);
+		assertFalse(cell.isDoorway());
+	}
+	
+	@Test
+	public void testNumberOfDoorways() {
+		int numDoors = 0;
+		for (int row = 0; row < board.getNumRows(); row++)
+			for (int col = 0; col < board.getNumColumns(); col++) {
+				BoardCell cell = board.getCell(row, col);
+				if (cell.isDoorway())
+					numDoors++;
+			}
+		Assert.assertEquals(16, numDoors);
+	}
 }
