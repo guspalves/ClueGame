ea00e8be5afc9a6c1fe3ad09f4dd365cddebc36a
guspalves <gus.alves@hotmail.com>
2021-03-05 15:14:52 -0700

    Adding test files to directory

---
+++ src/tests/BoardAdjTargetTest.java
@@ -0,0 +1,21 @@
+/**
+ * @author Gustavo Alves
+ * @author Noah Terry
+ * 
+ * Description: Testing to ensure algorithms for targets and adjancecy are working properly.
+ */
+
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BoardAdjTargetTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
